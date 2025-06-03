b373929b82f6580d1d6294a200961c652209109a
0xBondSUI <sugar@cetus.io>
2025-06-03 17:09:14 +0800

    Fix tests

--- tests/src/utils/api.ts
+++
@@ -1,6 +0,0 @@
-export function processEndpoint(endpoint: string): string {
-  if (endpoint.endsWith("/find_routes")) {
-    return endpoint.replace("/find_routes", "")
-  }
-  return endpoint
-}
--- tests/src/utils/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './contracts'
-export * from './msafe'
-export * from './api'
-export * from './coin'
-export * from './transaction'
