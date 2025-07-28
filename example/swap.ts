df4ca79e10d39287d6f3becb5eb1696e8f853de5
cevin <qixingyue@gmail.com>
2025-07-28 17:40:33 +0800

    Update swap.ts
    latest sdk should use endpoint params.

--- example/swap.ts
+++ example/swap.ts
@@ -1,9 +1,17 @@
-import { AggregatorClient } from "@cetusprotocol/aggregator-sdk"
+import { AggregatorClient, Env } from "@cetusprotocol/aggregator-sdk";
 import BN from "bn.js"
 
+
 async function main() {
-  // default to mainnet
-  const client = new AggregatorClient()
+  
+  const aggregatorURL = "https://api-sui.cetus.zone/router_v2/find_routes";
+  const client = new AggregatorClient(
+    {
+      endpoint: aggregatorURL,
+      // default to mainnet
+      env: Env.Mainnet
+    }
+  )
 
   const from = "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI"
   const target = "0x06864a6f921804860930db6ddbe2e16acdf8504495ea7481637a1c8b9a8fe54b::cetus::CETUS"
